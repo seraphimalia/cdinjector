29bbb9677c27fc18ab32e1deac4f3e46a7f3ebac
Clive Munro <23497305+theclive@users.noreply.github.com>
2021-12-28 19:12:12 +0200

    Update URL & Bump Version

--- chrome-extension/cdinjector.js
+++ chrome-extension/cdinjector.js
@@ -40,6 +40,7 @@ class CDInjector {
     this.emptySet = new Set()
 
     this.serverAddress = `https://cdn.jsdelivr.net/gh/seraphimalia/cm-eform-scripts@master/src/`
+    //this.serverAddress = `https://cdn.jsdelivr.net/gh/seraphimalia/cm-eform-scripts@master/`
     //this.serverAddress = `https://raw.githubusercontent.com/seraphimalia/cm-eform-scripts/master/src/`
     //this.serverAddress = `https://deploy-preview-1--cm-eform-scripts.netlify.app/`;
     //this.serverAddress = `https://testing.local.munro.co.za:8000/`
